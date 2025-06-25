536062e11f0ae2741e615568f179619bd1c063da
Aditya Ranjan <aditya74810@gmail.com>
2025-06-25 23:22:53 +0530

    updated link

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,23 +1,22 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = `http://localhost:8000`;
-
+// const API_URL = `http://localhost:8000`;
+const API_URL = `https://file-sharing-app-yr1c.onrender.com`;
 
 export const uploadFile = async (data) => {
-    try {
-        let response = await axios.post(`${API_URL}/upload`, data);
-        return response.data;
-    } catch (error) {
-        console.error('Error while calling the api', error.message);
-    }
-}
-
+  try {
+    let response = await axios.post(`${API_URL}/upload`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error while calling the api", error.message);
+  }
+};
 
 export const deleteFile = async (fileId) => {
-    try {
-         await axios.delete(`${API_URL}/delete-file/${fileId}`);
-        return 
-    } catch (error) {
-        console.error('Error while calling the api', error.message);
-    }
-}
+  try {
+    await axios.delete(`${API_URL}/delete-file/${fileId}`);
+    return;
+  } catch (error) {
+    console.error("Error while calling the api", error.message);
+  }
+};
