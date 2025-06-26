f668bf3b7c6df9667495d7e91fa8bcea71eceb7f
Aditya Ranjan <aditya74810@gmail.com>
2025-06-26 11:01:26 +0530

    improve code

--- client/src/component/Google.jsx
+++ client/src/component/Google.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 import { deleteFile, getFiles } from "../services/api";
 import "./Google.css"; // Import CSS
